b881e8cc5ad2587a965350763b38b61b3e98bc74
Daniel <meskal1@rambler.ru>
2022-10-01 13:13:43 +0300

    some_refactorings

--- index.js
+++ index.js
@@ -1,81 +1,57 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-    var buttonInnerHTML = this.innerHTML;
-
+document.querySelectorAll(".drum").forEach((el) => {
+  el.addEventListener("click", () => {
+    const buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-
     buttonAnimation(buttonInnerHTML);
-
   });
-
-}
-
-document.addEventListener("keypress", function(event) {
-
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
-
 });
 
+document.addEventListener("keypress", (event) => {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
-function makeSound(key) {
-
+const makeSound = (key) => {
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-
     case "a":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
-
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
+      var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
-
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
-
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
-
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
+      var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
-
-
-    default: console.log(key);
-
+    default:
+      console.log(key);
   }
-}
+};
 
-
-function buttonAnimation(currentKey) {
-
-  var activeButton = document.querySelector("." + currentKey);
-
-  activeButton.classList.add("pressed");
-
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
+const buttonAnimation = (currentKey) => {
+  const activeButton = document.querySelector("." + currentKey);
+  if (activeButton) activeButton.classList.add("pressed");
+  setTimeout(function () {
+    if (activeButton) activeButton.classList.remove("pressed");
   }, 100);
-
-}
+};
